234e5abcf7df3c3bf4d4d745fc926df0f1dc3857
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2023-02-13 18:34:39 +0530

    sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,75 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {  MatDialogModule } from "@angular/material/dialog";
+import {  MatInputModule } from "@angular/material/input";
+import {  MatListModule } from "@angular/material/list";
+import {  MatPaginatorModule } from "@angular/material/paginator";
+import {  MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import {  MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSortModule } from "@angular/material/sort";
+import {  MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {HttpClientModule} from "@angular/common/http";
+import { ReactiveFormsModule} from "@angular/forms";
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatRadioModule} from '@angular/material/radio';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatSliderModule} from '@angular/material/slider';
+import { MatTooltipModule} from '@angular/material/tooltip';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTreeModule} from '@angular/material/tree';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AboutComponent } from './component/about/about.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        MatTabsModule,
+        MatSidenavModule,
+        MatListModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatProgressSpinnerModule,
+        MatStepperModule,
+        MatDialogModule,
+        AppRoutingModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        DragDropModule,
+        MatRadioModule,
+        MatSliderModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        ReactiveFormsModule,
+        MatGridListModule,
+        MatTreeModule,
+        ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
